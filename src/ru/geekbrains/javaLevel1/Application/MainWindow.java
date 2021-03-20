a64c62416b112d5d6a4617778825c3a21a5cffa4
kilyala <herasav@gmail.com>
2021-03-20 14:35:11 +0300

    add clicker

--- src/ru/geekbrains/javaLevel1/Application/MainWindow.java
+++ src/ru/geekbrains/javaLevel1/Application/MainWindow.java
@@ -1,4 +1,41 @@
 package ru.geekbrains.javaLevel1.Application;
 
-public class MainWindow {
+import ru.geekbrains.javaLevel1.Main;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainWindow extends JFrame {
+    private int counter = 0;
+
+    public MainWindow() {
+        setTitle("Test Window");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setBounds(300, 300, 200, 200);
+        JButton button = new JButton(String.valueOf(counter));
+
+
+        button.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                counter++;
+                button.setText(String.valueOf(counter));
+            }
+        });
+
+
+        add(button, BorderLayout.CENTER);
+
+        setVisible(true);
+
+    }
+
+
+    public static void main(String[] args) {
+        new MainWindow();
+
+    }
 }
